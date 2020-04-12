1ade17f91cddb24c754fa5b459d8422fc534b5d9
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-04-12 18:05:45 +0200

    Check Email registed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,23 @@
+const User = require('../models/User')
 
+exports.userCreate = async (req, res) =>{
 
-exports.userCreate = (req, res) =>{
-        console.log('user_create')
+        const {email} = req.body
+        
+        try {
+                const user = new User (req.body)
+                
+                if(await User.findOne({email})){
+                        res.status(400).send({msg: 'Email has registed'})
+                        return
+                }else{
+                        await user.save()
+                        res.status(200).send({msg: 'User Created'})
+
+                }
+
+        } catch (error) {
+
+                res.status(400).send({msg: error})    
+        }
 }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 
 connectDB()
 
+app.use(express.json({ extended: true }))
+
 const PORT = process.env.PORT || 4000
 
 app.use('/api/user', require('./routes/user'))
