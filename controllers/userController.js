5a73dc210a5133cad3b0f718c5772d44c2fd5481
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-04-13 13:28:25 +0200

    hash Password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,22 @@
 const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
 
 exports.userCreate = async (req, res) =>{
 
-        const {email} = req.body
+        const {email, password} = req.body
         
         try {
                 const user = new User (req.body)
                 
                 if(await User.findOne({email})){
+                        
                         res.status(400).send({msg: 'Email has registed'})
                         return
                 }else{
+                        
+                        const salt = await bcryptjs.genSalt(10)
+                        user.password = await bcryptjs.hash(password, salt)
+
                         await user.save()
                         res.status(200).send({msg: 'User Created'})
 
